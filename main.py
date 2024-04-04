954614b3523744f99629a1b892645d4a019cf250
FIVe04 <ivan.a.frolov@gmail.com>
2024-04-05 00:56:03 +0300

    added ssl certificate for https

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from fastapi import FastAPI
 
-
 from contextlib import asynccontextmanager
 
 from database import create_tables, delete_tables
 
 from router import router as tasks_router
+
+import uvicorn
+
+import ssl
+
+import sys
+
 
 
 @asynccontextmanager
@@ -17,9 +23,16 @@ async def lifespan(app: FastAPI):
     print("Turning off")
 
 
-
 app = FastAPI(lifespan=lifespan)
 app.include_router(tasks_router)
+
+if __name__ == "__main__":
+    if len(sys.argv) == 3:
+        ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+        ssl_context.load_cert_chain(sys.argv[1], keyfile=sys.argv[2])
+        uvicorn.run(app, host="0.0.0.0", port=80, ssl=ssl_context)
+    else:
+        uvicorn.run(app, host="0.0.0.0", port=80)
 
 
 
